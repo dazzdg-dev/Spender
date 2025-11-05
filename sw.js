f33160ef95f8b926d7b8e685c9aab48cc7284530
dazzdg-dev <dazz.dg@gmail.com>
2025-11-05 11:43:34 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='tp-advisor-v1';
-self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll([
-    './','./index.html','./manifest.webmanifest'
-  ])));
-});
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-});
